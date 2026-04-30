4e857a6d612692c1cddb64757db642ca4e1869ab
Yomna-Xll <youmnaebrahim956@gmail.com>
2026-04-30 15:27:31 +0300

    Restore original work and add Firebase configuration

--- savoir/lib/main.dart
+++ savoir/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:savoir/features/home/presentation/screens/home_screen.dart';
 import 'package:savoir/features/authentication/presentation/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(Savoir());
 }
 
